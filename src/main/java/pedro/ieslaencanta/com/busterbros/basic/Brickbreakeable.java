a7909f3a083fa9b9da2da865029c43f6f1662ec8
DAWTarde <DAWTarde@10.2.1.177>
2023-04-19 17:35:19 +0200

    Empezada la practica

---
+++ src/main/java/pedro/ieslaencanta/com/busterbros/basic/Brickbreakeable.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pedro.ieslaencanta.com.busterbros.basic;
+
+/**
+ *
+ * @author DAWTarde
+ */
+public class Brickbreakeable extends Brick {
+
+    public Brickbreakeable(){
+        super();
+    }
+    public Brickbreakeable(double x, double y, double width, double height) {
+        super(x, y, width, height);
+    }
+}
